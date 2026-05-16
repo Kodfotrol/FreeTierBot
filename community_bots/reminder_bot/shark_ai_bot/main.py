9c839c56812f00a3a948e2ce4cb9f0a5757e68b3
Kodfotrol <103263935+Kodfotrol@users.noreply.github.com>
2026-05-16 15:59:06 +0300

    Create main.py

---
+++ community_bots/reminder_bot/shark_ai_bot/main.py
@@ -0,0 +1,7 @@
+import functions_framework
+from combined import app as flask_app
+
+@functions_framework.http
+def main(request):
+    """Точка входа для Google Cloud Function."""
+    return flask_app(request)
